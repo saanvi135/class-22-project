dc3547ae3df02679cf81b68608f178d041008f76
saanvi135 <85235091+saanvi135@users.noreply.github.com>
2021-08-31 08:53:13 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# class-22-project
+# Project_Template_24
---
+++ sketch.js
@@ -0,0 +1,87 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var palyer, playerBase;
+var computer, computerBase;
+
+var arrow;
+
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
+  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+ 
+  //Create Player Archer Object
+  playerArcher=new PlayerArcher(345,playerBase.body.position.y-180,120,120)
+
+computerBase=new ComputerBase(
+    width - 300,
+    random(450, height - 300),
+    180,
+    150
+  );
+  computer = new Computer(
+    width - 280,
+    computerBase.body.position.y - 153,
+    50,
+    180
+  );
+  computerArcher = new ComputerArcher(
+    width - 340,
+    computerBase.body.position.y - 180,
+    120,
+    120
+  );
+  
+  //Create an arrow Object
+  computerArrow=new ComputerArrow(width-345,computerBase.body.position.y-180,120,120)
+  playerArrow=new PlayerArrow(350,playerBase.body.position.y-180,120,120)
+}
+
+function draw() {
+  background(180);
+
+  Engine.update(engine);
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+
+ 
+  playerBase.display();
+  player.display();
+  
+
+  computerBase.display();
+  computer.display();
+  
+  playerArcher.display();
+  computerArcher.display()
+
+
+  //Display arrow();
+  computerArrow.display()
+  playerArrow.display()
+  
+  //if Space (32) key is pressed call shoot function of playerArrow
+  if(keyCode === 32){
+    //Call shoot() function and pass angle of playerArcher
+arrow.shoot(playerArcher.body.angle)
+  }
+  }
+  
+
+
+
+
